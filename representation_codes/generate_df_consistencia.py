de54b8b97d20968d0163bb9519a6217c63ff1563
Vinicius <vinicius.teixeira.carvalho@usp.br>
2024-12-25 13:10:29 -0300

    Initial but radical changes in generate_df_consistencia.py. The code is now modularized and more organic. There's still some things to do in the graph treatment.

--- representation_codes/generate_df_consistencia.py
+++ representation_codes/generate_df_consistencia.py
@@ -5,47 +5,44 @@ import numpy as np
 import networkx as nx
 import os
 from data_steps.generate_representations import get_representations
-
-""" Getting the hiperparameters for the experiments"""
-
-parser = argparse.ArgumentParser(description ='Generate the consistency datasets')
-parser.add_argument('--hyperparameters',  type=int, nargs=3, 
-                    help='Number of neighbors for semantic, geographical and temporal neighbors graph: k_s, k_g, k_t')
-parser.add_argument('--dataset_path', type=str, help='The name of the current dataset file') 
-args = parser.parse_args()
-
-k_s, k_g, k_t = args.hyperparameters
-dataset_path = args.dataset_path
-
-dataset_name = os.path.basename(dataset_path)
-
-output_dir = "./results/consistencia"
-Path(output_dir).mkdir(parents=True, exist_ok=True)
-
-"""Getting the representation components"""
-
-R_s_norm, R_g, R_t, _ = get_representations(dataset_path)
-
-""" Consistency graph as a way to measure the success of the representation"""
-
 from sklearn.neighbors import kneighbors_graph
+import pickle
 
-A_s = kneighbors_graph(R_s_norm, k_s, mode='connectivity',metric="cosine")
-A_g = kneighbors_graph(R_g, k_g, mode='connectivity',metric="haversine")
-A_t = kneighbors_graph(R_t, k_t, mode='connectivity',metric="euclidean")
+def get_consistency_adjacency(R_s: np.array, R_g: np.array, R_t: np.array, k_s: int, k_g: int, k_t: int) -> np.array:
+    # these kneighbors_graphs are directed
+    A_s = kneighbors_graph(R_s_norm, k_s, mode='connectivity',metric="cosine")
+    A_g = kneighbors_graph(R_g, k_g, mode='connectivity',metric="haversine")
+    A_t = kneighbors_graph(R_t, k_t, mode='connectivity',metric="euclidean")
+    
+    A_f = A_s.toarray() + A_g.toarray() + A_t.toarray()
+    A_f = np.where(A_f < 2, 0, 1) # we can do something as consider the reciprocity. Like if A + A^t = 2, then theres a edge
+    
+    return A_f
 
-# these kneighbors_graphs are directed
 
-A_f = A_s.toarray() + A_g.toarray() + A_t.toarray()
+if __name__ == "__main__":
+    output_dir = "./RL-COVID-EVENTS/results/consistencia"
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    
+    parser = argparse.ArgumentParser(description ='Generate the consistency datasets')
+    parser.add_argument('--hyperparameters',  type=int, nargs=3, 
+						help='Number of neighbors for semantic, geographical and temporal neighbors graph: k_s, k_g, k_t')
+    parser.add_argument('--dataset_path', type=str, help='The name of the current dataset file') 
+    args = parser.parse_args()
 
-A_f = np.where(A_f < 2, 0, 1)
+    k_s, k_g, k_t = args.hyperparameters
+    dataset_path = args.dataset_path
+ 
+    dataset_name = os.path.basename(dataset_path)
+    R_s_norm, R_g, R_t, _ = get_representations(dataset_path)
 
-G_consistencia =  nx.Graph(A_f) 
+    A_f = get_consistency_adjacency(R_s_norm, R_g, R_t, k_s, k_g, k_t) # this graph is also directed...
 
-""" Creating a dataframe to facilitate the comparsions """
+    G_consistency =  nx.Graph() # here, i should put nx.DiGraph()
 
-df_consistencia = pd.DataFrame([str(edge) for edge in G_consistencia.edges()], columns=['Edges'])
-
-df_consistencia.to_parquet(f'{output_dir}/df_consistencia_{dataset_name[:-4]}_{k_s}_{k_g}_{k_t}.parquet')
-
-print('success', flush=True)
+    edges = set(G_consistency.edges())
+    output_file = f"{output_dir}/edges_consistency_{dataset_name[:-4]}_{k_s}_{k_g}_{k_t}.pkl"
+    with open(output_file, 'wb') as f:
+        pickle.dump(edges, f)
+    
+    print(f"Edges saved successfully to {output_file}", flush=True)
